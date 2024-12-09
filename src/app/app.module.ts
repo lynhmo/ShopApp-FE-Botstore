c51542ca21108c0709569b73a3f5df8cdc6ff9ca
lynhmo <chinsugaming@gmail.com>
2024-12-09 17:04:52 +0700

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HomeComponent } from './page/home/home.component';
+import { ProductComponent } from './page/product/product.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
 import { AppComponent } from './app.component';
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'product', component: ProductComponent },
+  { path: '', redirectTo: '/', pathMatch: 'full' }, // Default route
+];
+
 
 @NgModule({
   declarations: [
+
+    HomeComponent,
+    ProductComponent,
+    HeaderComponent,
+    FooterComponent,
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes) // Configure the router
   ],
   providers: [],
   bootstrap: [AppComponent]
