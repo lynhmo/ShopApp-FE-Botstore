a0b2853d98e44f7d7d14bfaff7471ea5b24346bb
lynhmo <chinsugaming@gmail.com>
2024-12-27 00:02:51 +0700

    Cải tiến giao diện trang quản trị với bảng điều khiển bên trái và chức năng điều hướng về trang chủ

--- src/app/admin/component/admin-page/admin-page.component.ts
+++ src/app/admin/component/admin-page/admin-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-page',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-page.component.scss']
 })
 export class AdminPageComponent {
+  myDataArray: any;
+  constructor(private router: Router) { }
 
+  goToHome(): void {
+    this.router.navigate(['/']);
+  }
 }
